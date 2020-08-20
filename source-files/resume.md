daaef0118a6a576967ac2894dec69ea3f13907e1
mcpar-land <john@mcpar.land>
2020-08-20 13:25:51 -0500

    initial commit

---
+++ source-files/resume.md
@@ -0,0 +1,102 @@
+# Skills
+
+- React (HTML / CSS / Javascript)
+- Typescript
+- Node
+- SCSS / Sass / Less
+- Bootstrap / React Bootstrap
+- Material UI
+- Docker
+- Rust
+- Unity (C#)
+- Unreal Engine
+- Git / Github / Github Actions
+- Photoshop
+
+# Experience
+
+<div class="two-columns" id="jobs">
+
+<div>
+
+## Director of Curriculum, Code Ninjas
+
+Date and Month
+
+- One
+- Two
+- Three
+
+</div>
+
+<div>
+
+## Tech Camp Instructor, iD Tech
+
+June - July 2017, June - August 2019
+
+- First
+- Second
+- Third
+
+</div>
+
+<div>
+
+## Other Job
+
+Date and Month
+
+- One bullet point here
+- Another bullet point around there
+
+</div>
+
+</div>
+
+<div class="two-columns">
+
+<div>
+
+# Education
+
+Strake Jesuit College Perparatory, Houston, 2014
+
+- AP Object-Oriented Programming
+
+University of Utah, 2019 - BFA, Game Design
+
+- Intro to Object-Oriented Programming, 2014
+- Intro to Algorithms and Data, 2015
+- Digital Visual Effects, 2015
+- 3D Modeling, 2016
+- Film Production, 2018
+- Alternative Game Development, 2018
+- 3D Rigging, 2019
+- Senior Capstone Project, 2019
+
+Self-Teaching, 2009 - Present
+
+</div>
+
+<div>
+
+# Certifications
+
+- Triplebyte
+
+# Projects
+
+Project One
+
+- A
+- B
+
+Project Two
+
+- C
+- D
+
+</div>
+
+</div>
---
+++ src/index.js
@@ -0,0 +1,56 @@
+require('dotenv-flow').config()
+const markdownIt = require('markdown-it')
+const htmlPdf = require('pdf-puppeteer')
+const handlebars = require('handlebars')
+const fs = require('fs')
+const sass = require('node-sass')
+const path = require('path')
+const prettier = require('prettier')
+
+const converter = markdownIt({
+	html: true,
+	xhtmlOut: true,
+	typographer: true,
+})
+
+const templateSource = fs.readFileSync('template/template.html').toString()
+const template = handlebars.compile(templateSource)
+
+const mdToPdf = (inPath) => {
+	const md = fs.readFileSync(inPath).toString()
+	console.log('Converting Markdown to HTML...')
+	const bodyHtml = converter.render(md)
+	console.log('Converting SASS...')
+	const css = sass
+		.renderSync({
+			file: 'template/styles.scss',
+		})
+		.css.toString()
+	console.log('Populating template...')
+	const telFormatted = [
+		process.env.TEL.substr(0, 3),
+		process.env.TEL.substr(3, 3),
+		process.env.TEL.substr(6),
+	].join('.')
+	let html = template({
+		body: bodyHtml,
+		tel: process.env.TEL,
+		telFormatted,
+		addr: process.env.ADDR,
+		css,
+	})
+	const filename = path.basename(inPath).split('.')[0]
+	console.log('Formatting with prettier...')
+	html = prettier.format(html, { parser: 'html', useTabs: true })
+	console.log('Exporting to HTML...')
+	fs.writeFileSync(`exports/${filename}.html`, html)
+	console.log('Exporting to PDF...')
+	// htmlPdf.create(html).toFile(outPath, (err, res) => {
+	// 	console.log(`Exported ${res.filename}`)
+	// })
+	htmlPdf(html, (data) => {
+		fs.writeFileSync(`exports/${filename}.pdf`, data)
+	})
+}
+
+mdToPdf('./source-files/resume.md', './exports/resume.pdf')
